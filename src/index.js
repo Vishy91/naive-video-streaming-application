b49034024c941514d7eb209e7ab9848f2ca88f97
vishy91 <vaishnavi.mukundhan@gmail.com>
2018-08-15 22:22:13 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash'
+const API_KEY = 'AIzaSyCVPEEUrHD4MTSf4jmslLvawF4kiu7rNeY';
+
+
+
+
+class App extends Component{
+constructor(props){
+  super(props);
+  this.state= {videos: [], selectedVideo:null};
+  this.videoSearch('redux')
+
+}
+  videoSearch(term){
+    YTSearch({key: API_KEY, term}, (videos) => {
+      this.setState({ videos:videos,
+      selectedVideo:videos[0] });
+    });
+  }
+  render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},500);
+    return (
+      <div>
+        <SearchBar onSearchTermChange= {videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videos={this.state.videos}
+         onVideoSelect={selectedVideo => this.setState({selectedVideo}) }/>
+      </div>
+    );
+  }
+}
+
+
+//put it on the page(in DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
